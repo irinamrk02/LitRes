c145fee6ebb3a305f7efad012e8d1dc1f52baccc
Ирина Марковщенкова <irinamrk02@mail.ru>
2020-05-23 18:39:18 +0300

    Добавьте файлы проекта.

---
+++ LitRes/Main.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace LitRes
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonClient = new System.Windows.Forms.Button();
+            this.buttonAmin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LitRes.Properties.Resources.litres_logo_rastr;
+            this.pictureBox1.Location = new System.Drawing.Point(-37, -13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(494, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonClient
+            // 
+            this.buttonClient.BackColor = System.Drawing.Color.OrangeRed;
+            this.buttonClient.Font = new System.Drawing.Font("Roboto", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClient.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonClient.Location = new System.Drawing.Point(12, 151);
+            this.buttonClient.Name = "buttonClient";
+            this.buttonClient.Size = new System.Drawing.Size(390, 84);
+            this.buttonClient.TabIndex = 1;
+            this.buttonClient.Text = "Клиент";
+            this.buttonClient.UseVisualStyleBackColor = false;
+            this.buttonClient.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // buttonAmin
+            // 
+            this.buttonAmin.BackColor = System.Drawing.Color.OrangeRed;
+            this.buttonAmin.Font = new System.Drawing.Font("Roboto", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAmin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonAmin.Location = new System.Drawing.Point(12, 248);
+            this.buttonAmin.Name = "buttonAmin";
+            this.buttonAmin.Size = new System.Drawing.Size(390, 84);
+            this.buttonAmin.TabIndex = 2;
+            this.buttonAmin.Text = "Администратор";
+            this.buttonAmin.UseVisualStyleBackColor = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(418, 354);
+            this.Controls.Add(this.buttonAmin);
+            this.Controls.Add(this.buttonClient);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "ЛитРес";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonClient;
+        private System.Windows.Forms.Button buttonAmin;
+    }
+}
+
